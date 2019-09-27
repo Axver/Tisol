ed1fd55c25c32997f79e9c566f2fde934cf8c628
unknown <ecimoramora@gmail.com>
2019-09-27 08:38:40 +0700

    first commit

---
+++ app/Views/template/header.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+    <title>Sssoon Page </title>
+
+    <link href="<?php echo base_url('/assets/css/bootstrap.css') ?>" rel="stylesheet" />
+    <link href="<?php echo base_url('/assets/css/coming-sssoon.css') ?>" rel="stylesheet" />
+
+    <!--     Fonts     -->
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Grand+Hotel' rel='stylesheet' type='text/css'>
+    <script src="<?php echo base_url('/assets/js/jquery-1.10.2.js') ?>" type="text/javascript"></script>
+    <script src="<?php echo base_url('/assets/js/bootstrap.min.js') ?>" type="text/javascript"></script>
+
+</head>
---
+++ assets/js/flight.js
@@ -0,0 +1,79 @@
+function searchFlight() {
+    let from=$("#from").val();
+    let destination=$("#destination").val();
+    let date=$("#date").val();
+
+    console.log(from);
+    console.log(destination);
+    console.log(date);
+}
+
+$(function() {
+    $( '.autocomplete' ).each(function () {
+        var apca = new apc('autocomplete', {
+            key : '689e66e8e2',
+            //secret : 'xxxxxxxxxxxxxxx', // Your API Secret Key: use this if you are not connecting from a web server
+            limit : 7
+        });
+
+        var dataObj = {
+            source: function( request, response ) {
+                // make the request
+                apca.request( request.term );
+
+                // this builds each line of the autocomplete
+                itemObj = function (airport, isChild) {
+                    var label;
+                    if (isChild) { // format children labels
+                        label = '&rdsh;' + airport.iata + ' - ' + airport.name;
+                    } else { // format labels
+                        label = airport.city;
+                        if (airport.state.abbr) {
+                            label += ', ' + airport.state.abbr;
+                        }
+                        label += ', ' + airport.country.name;
+                        label += ' (' + airport.iata + ' - ' + airport.name + ')';
+                    }
+                    return {
+                        label: label,
+                        value: airport.iata + ' - ' + airport.name,
+                        code: airport.iata
+                    };
+                };
+
+                // this deals with the successful response data
+                apca.onSuccess = function (data) {
+                    var listAry = [],
+                        thisAirport;
+                    if (data.status) { // success
+                        for (var i = 0, len = data.airports.length; i < len; i++) {
+                            thisAirport = data.airports[i];
+                            listAry.push(itemObj(thisAirport));
+                            if (thisAirport.children) {
+                                for (var j = 0, jLen = thisAirport.children.length; j < jLen; j++) {
+                                    listAry.push(itemObj(thisAirport.children[j], true));
+                                }
+                            }
+                        }
+                        response(listAry);
+                    } else { // no results
+                        response();
+                    }
+                };
+                apca.onError = function (data) {
+                    response();
+                    console.log(data.message);
+                };
+            },
+            select: function( event, ui ) {
+                // do something for click event
+                console.log(ui.item.code);
+            }
+        }
+
+        // this is necessary to allow html entities to display properly in the jqueryUI labels
+        $(this).autocomplete(dataObj).data("ui-autocomplete")._renderItem = function( ul, item) {
+            return $('<li></li>').data('item.autocomplete', item ).html( item.label ).appendTo( ul );
+        };
+    });
+});
